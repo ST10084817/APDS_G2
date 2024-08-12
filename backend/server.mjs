fdf6f00fade1748d8770182c4e93bf7f10580f0c
st10084817 <st10084817@vcconnect.edu.za>
2024-08-12 09:19:28 +0200

    init

---
+++ backend/server.mjs
@@ -0,0 +1,36 @@
+import https from "https";
+import fs from "fs";
+import posts from "./routes/post.mjs";
+import users from "./routes/user.mjs";
+import express from "express";
+import cors from "cors";
+
+const PORT = 3001;
+const app = express();
+
+const options = {
+    key: fs.readFileSync('keys/privatekey.pem'),
+    cert: fs.readFileSync('keys/certificate.pem')
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.use((reg, res,next)=>
+{
+    res.setHeader('Access-Control-Allow-Orgin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    next();
+})
+
+app.use("/post", posts);
+app.route("/post", posts);
+app.use("/user", users);
+app.route("/user", users);
+
+
+//start the Express server
+let server = https.createServer(options, app)
+console.log(PORT)
+server.listen(PORT);
